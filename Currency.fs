f837f5c81a21d151dccadff9db35c69792e81253
Andrew Meier <ameier38@gmail.com>
2018-09-02 15:24:57 -0400

    add modules to Currency

--- Currency.fs
+++ Currency.fs
@@ -2,5 +2,6 @@ module Currency
 
 /// US dollar
 [<Measure>] type USD
-
-let usd (amt:decimal) = amt * 1m<USD>
+module USD =
+    let fromDecimal (amt:decimal) = amt * 1m<USD>
+    let toDecimal (amt:decimal<USD>) = amt / 1m<USD>
