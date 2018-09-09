085bd393214c8015e5ab4be74e9f9a53f4820565
Andrew Meier <ameier38@gmail.com>
2018-09-09 17:01:36 -0400

    make currency require qualified access

--- Currency.fs
+++ Currency.fs
@@ -2,6 +2,7 @@ module Currency
 
 /// US dollar
 [<Measure>] type USD
+[<RequireQualifiedAccess>]
 module USD =
     let fromDecimal (amt:decimal) = amt * 1m<USD>
     let toDecimal (amt:decimal<USD>) = amt / 1m<USD>
